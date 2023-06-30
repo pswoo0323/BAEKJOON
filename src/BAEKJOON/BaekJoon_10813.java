4ee52317cbcddb65912b33fa46381a4005d849ea
SangWoo Park <psw8554@gmail.com>
2023-06-30 20:21:37 +0900

    first commit

---
+++ src/BAEKJOON/BaekJoon_10813.java
@@ -0,0 +1,28 @@
+package BAEKJOON;
+import java.util.Scanner;
+import java.io.*;
+public class BaekJoon_10813 {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int arr[] = new int[N];
+        int M = sc.nextInt();
+        int temp;
+
+        for(int i =0; i<arr.length; i++){
+            arr[i] = i+1;
+        }
+        for(int j=0; j<M; j++){
+            int I = sc.nextInt();
+            int J = sc.nextInt();
+
+            temp = arr[I-1];
+            arr[I-1] = arr[J-1];
+            arr[J-1] = temp;
+        }
+        for(int k=0; k<arr.length; k++){
+            System.out.print(arr[k] + " ");
+        }
+    }
+}
