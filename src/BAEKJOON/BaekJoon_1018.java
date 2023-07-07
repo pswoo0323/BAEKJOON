a3c71f33ffbfba9fddb16c25682db155e6e3b6ba
SangWoo Park <psw8554@gmail.com>
2023-07-07 11:14:04 +0900

    0707

---
+++ src/BAEKJOON/BaekJoon_1018.java
@@ -0,0 +1,56 @@
+package BAEKJOON;
+import java.util.Scanner;
+public class BaekJoon_1018 {
+    public static boolean[][] arr;
+    public static int min = 64;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int M = in.nextInt();
+
+        arr = new boolean[N][M];
+        //배열
+        for(int i=0; i<N; i++){
+            String str = in.next();
+
+            for(int j=0; j<M; j++){
+                if(str.charAt(j)=='w'){
+                    arr[i][j] = true;
+                } else{
+                    arr[i][j] = false;
+                }
+            }
+        }
+
+        int N_row = N-7;
+        int M_col = M-7;
+
+        for(int i=0; i<N_row; i++){
+            for(int j=0; j< M_col; j++){
+                find(i,j);
+            }
+        }
+        System.out.println(min);
+    }
+    public static void find(int x, int y){
+        int end_x = x+8;
+        int end_y = y+8;
+        int count = 0;
+
+        boolean TF = arr[x][y];
+
+        for(int i = x; i<end_x; i++){
+            for(int j=y; j<end_y; j++){
+                //옳바른 색이 아닐경이 count 1증가
+                if(arr[i][j] != TF){
+                    count++;
+                }
+              TF = (!TF);
+            }
+            TF = !TF;
+        }
+        count = Math.min(count, 64-count);
+        min = Math.min(min,count);
+    }
+}
---
+++ src/BAEKJOON/BaekJoon_1181.java
@@ -0,0 +1,46 @@
+package BAEKJOON;
+
+import java.util.Arrays;
+import java.util.Comparator;//compare 메소드는 양의정수,0,음의정수 총 3가지 리턴 값이 있다.
+import java.util.Scanner;
+//정렬문제
+public class BaekJoon_1181 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+
+        //개행버림
+        sc.nextLine();
+
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextLine();
+        }
+
+        Arrays.sort(arr,new Comparator<String>() {
+            public int compare(String s1, String s2){
+                //단어가 같을 경우
+                if (s1.length()==s2.length()) {
+                    return s1.compareTo(s2);//사전 순 정렬, compareTo : 두개의 값을 비교하여 int값으로 봔환해주는 함수
+                }
+                else{
+                    return s1.length() - s2.length();
+                }
+            }
+        });
+        //미리 첫 번째 값을 출력
+        System.out.println(arr[0]);
+        //i-1 즉 0-1=-1 이 되니깐 조건문이 돌아가지 않음
+        //그래서 1-1 해주기 위해 미리 0의 위치 출력
+
+        //중족되지 않는 단어만 출력
+        for(int i=1; i<n; i++){
+            if(!arr[i].equals(arr[i-1])){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
+
