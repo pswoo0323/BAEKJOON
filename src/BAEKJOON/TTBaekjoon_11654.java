a98f6b4d26abf57b683e02e55a456bf526f700f0
Park Sang Woo <128164231+pswoo0323@users.noreply.github.com>
2023-08-21 21:09:18 +0900

    Delete src directory

--- src/BAEKJOON/TTBaekjoon_11654.java
+++
@@ -1,9 +0,0 @@
-package BAEKJOON;
-import java.util.Scanner;
-public class TTBaekjoon_11654 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.next().charAt(0);
-        System.out.println(N);
-    }
-}
