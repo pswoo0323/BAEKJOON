0afae662537b2f138912c446e5c5a12d7bb2432c
SangWoo Park <psw8554@gmail.com>
2023-08-21 21:10:19 +0900

    백준 알고리즘 문제풀이

---
+++ BAEKJOON/src/BAEKJOON/Baekjoon_2501_B.java
@@ -0,0 +1,27 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_2501_B {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int count = 0;
+        int t = 0;
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                count++;
+            }
+            if (count == K) {
+                t = i;
+                break;
+            }
+        }
+        System.out.println(t);
+    }
+}
